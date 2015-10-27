cef246895c925761fc76184053078ca33ac9819a
Álvaro Cutrim <alvaro.cutrim@gmail.com>
2015-10-27 03:50:20 -0300

    Atualização das classes de cadastro de endereço

---
+++ br/com/taipanet/model/Contribuinte.java
@@ -0,0 +1,110 @@
+package br.com.taipanet.model;
+
+import java.util.ArrayList;
+
+public class Contribuinte {
+	private long id;
+	private String nomeRazao;
+	private String cpfCnpj;
+	private TipoDocumento tipoDocumento;
+	private String numeroDocumento;
+	private String orgaoExpedidor;
+	private Estado ufExpedidor;
+	private String dataExpedicao;
+	private EstadoCivil estadoCivil;
+	private ArrayList<Endereco> enderecos;
+	private ArrayList<Contato> contatos;
+	
+	public Contribuinte() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeRazao() {
+		return nomeRazao;
+	}
+
+	public void setNomeRazao(String nomeRazao) {
+		this.nomeRazao = nomeRazao;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public TipoDocumento getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(TipoDocumento tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+	public String getNumeroDocumento() {
+		return numeroDocumento;
+	}
+
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+
+	public String getOrgaoExpedidor() {
+		return orgaoExpedidor;
+	}
+
+	public void setOrgaoExpedidor(String orgaoExpedidor) {
+		this.orgaoExpedidor = orgaoExpedidor;
+	}
+
+	public Estado getUfExpedidor() {
+		return ufExpedidor;
+	}
+
+	public void setUfExpedidor(Estado ufExpedidor) {
+		this.ufExpedidor = ufExpedidor;
+	}
+
+	public String getDataExpedicao() {
+		return dataExpedicao;
+	}
+
+	public void setDataExpedicao(String dataExpedicao) {
+		this.dataExpedicao = dataExpedicao;
+	}
+
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public ArrayList<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(ArrayList<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public ArrayList<Contato> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(ArrayList<Contato> contatos) {
+		this.contatos = contatos;
+	}
+	
+}
---
+++ br/com/taipanet/model/Estado.java
@@ -0,0 +1,35 @@
+package br.com.taipanet.model;
+
+public class Estado {
+	private long id;
+	private String descricao;
+	private String uf;
+	
+	public Estado() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+}
